ff0c25b6580017dbe0470f50e717130c4e680c1c
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-09-04 23:33:05 +0300

    +

--- src/test/java/ApachePOI/_02_ApachePOI_Nedir.java
+++ src/test/java/ApachePOI/_02_ApachePOI_Nedir.java
@@ -5,41 +5,49 @@ import java.util.Arrays;
 
 public class _02_ApachePOI_Nedir {
     /**
-     *  Interview SORUSU
+     * Interview SORUSU
      * 1- Excelden veri okumasını nasıl yaparsınız ?
-     *    Apache.POI ile yapıyorum.
-     *
+     * Apache.POI ile yapıyorum.
+     * <p>
      * 2- ApachePOI nedir ?
-     *    Excelden veri okuma aracı, kütüphanesi
-     *
+     * Excelden veri okuma aracı, kütüphanesi
+     * <p>
      * 3- ApachePOI yi nasıl kullanıyorsun ?
-     *    FileInputStream oluşturuyorum. (Dosyayı yani Exceli okuma için açma)
-     *    FileInputStream -> Workbook -> Sheet -> row -> cell ilşki sırasına göre okuyorum.
+     * FileInputStream oluşturuyorum. (Dosyayı yani Exceli okuma için açma)
+     * FileInputStream -> Workbook -> Sheet -> row -> cell ilşki sırasına göre okuyorum.
      */
 
 
     public static void main(String[] args) {
-        int [] x={1,2,3,4,5,6};
-        ArrayList<Integer> M=method(x);
+        int[] x = {1, 2, 3, 4, 5, 6};
+        ArrayList<Integer> M = method(x);
         System.out.println("M = " + M);
-        int [] d=methd(x);
+        int[] d = methd(x);
         System.out.println(Arrays.toString(d));
         System.out.println(fact(5));
+        System.out.println(summing(5, 3, 4, 5, 6, 7, 8));
+
 
     }
+
     public static ArrayList<Integer> method(int[] a) {
-        ArrayList<Integer>m = new ArrayList<>();
+        ArrayList<Integer> m = new ArrayList<>();
         for (int i = 0; i < a.length; i++) {
-            if (a[i]%2==0) {m.add(a[i]);}
+            if (a[i] % 2 == 0) {
+                m.add(a[i]);
+            }
         }
         return m;
     }
+
     public static int[] methd(int[] b) {
-        int[]list = new int[b.length/2];
-        int j=0;
+        int[] list = new int[b.length / 2];
+        int j = 0;
         for (int i = 0; i < b.length; i++) {
-            if(b[i]%2==1)
-            { list[j]=b[i]; j++; }
+            if (b[i] % 2 == 1) {
+                list[j] = b[i];
+                j++;
+            }
 
             /*for (int k : b) {
             if (k % 2 == 1) {
@@ -48,13 +56,23 @@ public class _02_ApachePOI_Nedir {
         }
         return list;
     }
-    public static int fact(int x){
-        int factMultiply=1;
-        while(x>0){
-        factMultiply *= x;
-        x--;
+
+    public static int fact(int x) {
+        int factMultiply = 1;
+        while (x > 0) {
+            factMultiply *= x;
+            x--;
         }
         return factMultiply;
     }
+
+    public static int summing(int... c) {
+        int sum = 0;
+        for (int i = 0; i < c.length; i++) {
+            sum += c[i];
+        }
+        return sum;
+    }
 }
+
 
