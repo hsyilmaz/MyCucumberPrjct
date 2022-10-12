1d3f7bc1759e48187503be8b56dfad0bbc8ecab7
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-10-12 21:23:52 +0300

    Jenkins cucumber report tests with profiles and new GWD setting

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@SmokeTest", // @Regression hangi senaryolarda bu etiket varsa onlar çalıştırılacak
         features = {"src/test/java/FeatureFiles/"},
         glue = {"StepDefinitions"}
-        //plugin = {"html:target//cucumber-reports.html"}
+        //plugin = {"html:target/cucumber-reports.html"}
         ,plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
 )
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
