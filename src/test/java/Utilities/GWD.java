e159d32bff8a311e6619ab14c30503b6a2c4a1e8
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-10-12 21:33:02 +0300

    final changes in GWD setting

--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -48,10 +48,13 @@ public class GWD {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
 
-                    ChromeOptions options=new ChromeOptions();
-                    options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1400,2400");
-                    threadDriver.set(new ChromeDriver(options)); // bu thread e chrome istenmişşse ve yoksa bir tane ekleniyor
-
+                    if(!runningFromIntelliJ()) {
+                        ChromeOptions options = new ChromeOptions();
+                        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1400,2400");
+                        threadDriver.set(new ChromeDriver(options)); // bu thread e chrome istenmişşse ve yoksa bir tane ekleniyor
+                    }
+                    else
+                        threadDriver.set(new ChromeDriver());
                     break;
 
                 case "firefox":
@@ -93,6 +96,11 @@ public class GWD {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+    public static boolean runningFromIntelliJ()
+    {
+        String classPath = System.getProperty("java.class.path");
+        return classPath.contains("idea_rt.jar");
     }
 
 
